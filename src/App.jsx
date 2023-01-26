904441649106a56c11b273effe4c8b7648a3fb98
Losto <frankhx0_18@hotmail.com>
2023-01-26 16:56:28 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Login from './pages/Login';
 
-
-
-
-
 function App() {
-    return <div className='fondo h-screen w-full flex justify-center items-center font-poppins'>
-      <Login/>
-    </div>;
+  return (
+    <div className='fondo h-screen w-full flex justify-center items-center font-poppins'>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
